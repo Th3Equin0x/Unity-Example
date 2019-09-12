59797df87601caa0399c78d5e40c9ad30df0613c
Th3Equin0x <sgruver22@gmail.com>
2019-09-12 13:51:03 -0500

    Competed the Code Challenge
    Imported the assets from the Asset Store, corrected the errors in code, added a script to make the plane's propeller rotate.

---
+++ Code Challenge 1 - Plane Programming/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,28 @@
+﻿/*
+* Stephen Gruver
+* FollowPlayerX.cs
+* Assignment 3
+* Instructs the camera to follow the plane (Player) so that it stays in view.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3(30, 0, 10);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Code Challenge 1 - Plane Programming/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,29 @@
+﻿/*
+* Stephen Gruver
+* SpinPropellerX.cs
+* Assignment 3
+* Rotates the plane's propeller in the Z-axis to simulate a real plane's propeller.
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+    }
+}
